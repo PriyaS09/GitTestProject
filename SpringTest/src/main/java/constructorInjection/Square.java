e679e6be9f9d23043296a4be2b913ae4f63ef8d2
prika <prika@192.168.0.102>
2019-10-19 12:01:04 +0530

    square parameter change

--- SpringTest/src/main/java/constructorInjection/Square.java
+++ SpringTest/src/main/java/constructorInjection/Square.java
@@ -2,17 +2,17 @@ package constructorInjection;
 
 public class Square implements Shape {
 
-	private int side;
+	private int side1;
 
-	public Square(int side) {
+	public Square(int side1) {
 		super();
-		this.side = side;
+		this.side1 = side1;
 	}
 
 	@Override
 	public int calculateArea() {
 		// TODO Auto-generated method stub
-		return side * side;
+		return side1 * side1;
 	}
 
 }
